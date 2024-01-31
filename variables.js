f3907057385aecefec41690bb604ec11c352e632
IlOvecompUter1$t <shahva2022@gmail.com>
2024-01-31 22:06:41 +0500

    variables

---
+++ variables.js
@@ -0,0 +1,53 @@
+// let inbox = 100;
+
+// alert(inbox)
+
+
+
+// let inbox = 99;
+
+// alert (inbox + 5)
+
+
+
+// let inbox1 = 53;
+// let inbox2 = 40;
+
+// let AllInbox = inbox1+inbox2;
+
+// alert(AllInbox)
+
+
+
+
+// let inbox = 40;
+
+// inbox = 50;
+
+// alert(inbox);
+
+
+
+//################ false #################
+// const inbox = 40;
+
+// inbox = 50;
+
+// alert(inbox);
+
+
+
+
+// let inbox = 40;
+
+// alert(inbox);
+
+// inbox = 50;
+
+
+// camelcase
+// let YourVideos = 300
+
+
+
+
